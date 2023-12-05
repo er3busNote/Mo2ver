1de9c6fc5c1de4c84b3809f90ce91ba6d7c6a0cc
er3busNote <qudwn0768@naver.com>
2023-12-06 05:19:53 +0900

    fix : config env → front

--- front/src/index.tsx
+++ front/src/index.tsx
@@ -5,6 +5,8 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+console.log(process.env.REACT_APP_API_URL);
+
 const container = document.getElementById('root') as HTMLElement;
 const root = ReactDOM.createRoot(container);
 root.render(
