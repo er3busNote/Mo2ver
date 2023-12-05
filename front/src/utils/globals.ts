f85cd66422b81ade3741cc8a88a6c81afda2bca2
er3busNote <qudwn0768@naver.com>
2023-12-06 05:07:12 +0900

    fix : config env → front

--- front/src/utils/globals.ts
+++ front/src/utils/globals.ts
@@ -1,4 +1,11 @@
 declare const REACT_APP_API_URL: string;
-const _REACT_APP_API_URL = process.env.REACT_APP_API_URL || REACT_APP_API_URL;
+let DEV_REACT_APP_API_URL = undefined;
+let PROD_REACT_APP_API_URL = undefined;
+if (process) {
+	DEV_REACT_APP_API_URL = process.env.REACT_APP_API_URL;
+} else {
+	PROD_REACT_APP_API_URL = REACT_APP_API_URL;
+}
+const _REACT_APP_API_URL = DEV_REACT_APP_API_URL || PROD_REACT_APP_API_URL;
 console.log(_REACT_APP_API_URL);
 export { _REACT_APP_API_URL as REACT_APP_API_URL };
