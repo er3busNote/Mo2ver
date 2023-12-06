a722200f2b74d6c2e0ab19b80ef1b14ca52946ff
er3busNote <qudwn0768@naver.com>
2023-12-07 08:17:19 +0900

    feat: csrf enabled → back

--- back/src/main/java/com/mo2ver/master/global/configs/AppConfig.java
+++ back/src/main/java/com/mo2ver/master/global/configs/AppConfig.java
@@ -33,7 +33,7 @@ public class AppConfig {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 
-    @Profile("development")    // → 개발환경(development)
+    @Profile({"dev", "development"})    // → 개발환경(development)
     @Bean
     public ApplicationRunner applicationRunner() {
         return new ApplicationRunner() {
