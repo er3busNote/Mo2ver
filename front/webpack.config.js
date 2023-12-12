02cd2524781a028f75c0d608cc1b2b69420c45f0
er3busNote <qudwn0768@naver.com>
2023-12-13 06:19:26 +0900

    fix: webpack config → front

--- front/webpack.config.js
+++ front/webpack.config.js
@@ -5,68 +5,71 @@ const Dotenv = require('dotenv-webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const devMode = process.env.NODE_ENV !== 'production';
-const baseURL = process.env.REACT_APP_API_URL;
 
-module.exports = {
-    mode: process.env.NODE_ENV , // → development / production
-    entry: './src/index.tsx',
-    devtool: devMode ? 'source-map' : false,
-    resolve: {
-        extensions: ['.ts', '.tsx', '.js'], // node_module → .js 필수!
-    },
-    output: {
-        path: path.resolve(__dirname, './dist'),
-        publicPath: './', // → 상대경로 설정
-        filename: 'js/[name].bundle.js',
-    },
-    module: {
-        rules: [
-            {
-                test: /\.ts(x?)$/,
-                use: 'ts-loader',
-            },
-            {
-                test: /\.css$/,
-                use: [ 'style-loader', 'css-loader'],
-            },
-            {
-                test: /\.svg$/,
-                use: ['@svgr/webpack', 'url-loader'],
-            },
-        ],
-    },
-    devServer: {
-        host: 'localhost',
-        port: port,
-        proxy: {
-            '/api': {
-                target: baseURL,
-                pathRewrite: { 
-                    "^/api": ""
+module.exports = (env, argv) => {
+    const devMode = argv.mode !== 'production';
+    const baseURL = env.REACT_APP_API_URL;
+
+    return {
+        mode: devMode ? 'development' : 'production', // → development / production
+        entry: './src/index.tsx',
+        devtool: devMode ? 'source-map' : false,
+        resolve: {
+            extensions: ['.ts', '.tsx', '.js'], // node_module → .js 필수!
+        },
+        output: {
+            path: path.resolve(__dirname, './dist'),
+            publicPath: './', // → 상대경로 설정
+            filename: 'js/[name].bundle.js',
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.ts(x?)$/,
+                    use: 'ts-loader',
                 },
-                changeOrigin: true,
-            }
+                {
+                    test: /\.css$/,
+                    use: [ 'style-loader', 'css-loader'],
+                },
+                {
+                    test: /\.svg$/,
+                    use: ['@svgr/webpack', 'url-loader'],
+                },
+            ],
         },
-        static: {
-            directory: path.join(__dirname, "dist"),
+        devServer: {
+            host: 'localhost',
+            port: port,
+            proxy: {
+                '/api': {
+                    target: baseURL,
+                    pathRewrite: { 
+                        "^/api": ""
+                    },
+                    changeOrigin: true,
+                }
+            },
+            static: {
+                directory: path.join(__dirname, "dist"),
+            },
         },
-    },
-    plugins: [
-        new Dotenv({
-            path: devMode ? './.env.development' : './.env.production',
-        }),
-        new HtmlWebpackPlugin({
-            template: './public/index.html', // public/index.html 파일을 읽음
-            filename: 'index.html', // output으로 출력할 파일은 index.html
-            indect: true,
-        }),
-        new MiniCssExtractPlugin({
-            filename: devMode ? 'css/[name].css' : 'css/[name].[hash].css',
-            chunkFilename: devMode ? 'css/[id].css' : 'css/[id].[hash].css',
-        }),
-        new webpack.DefinePlugin({  // → 배포 시, 환경설정 가져오기
-            REACT_APP_API_URL: JSON.stringify(baseURL),
-        }),
-    ],
+        plugins: [
+            new Dotenv({
+                path: devMode ? './.env.development' : './.env.production',
+            }),
+            new HtmlWebpackPlugin({
+                template: './public/index.html', // public/index.html 파일을 읽음
+                filename: 'index.html', // output으로 출력할 파일은 index.html
+                indect: true,
+            }),
+            new MiniCssExtractPlugin({
+                filename: devMode ? 'css/[name].css' : 'css/[name].[hash].css',
+                chunkFilename: devMode ? 'css/[id].css' : 'css/[id].[hash].css',
+            }),
+            new webpack.DefinePlugin({  // → 배포 시, 환경설정 가져오기
+                REACT_APP_API_URL: JSON.stringify(baseURL),
+            }),
+        ],
+    }
 };
